bb0c65a06f1844fb615743b8ce77828681e3f5ea
Placidoo <joedmichael.jm@gmail.com>
2024-11-24 03:21:10 +0000

    fix

--- roomba.c
+++ roomba.c
@@ -1,73 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // NOTE: Array[Yaxis][Xaxis]
 
-void mapLayout();
-void movePlayer(char movement, int mazeMap[][5], int currentPosition[2]);
+// Global Variables
+int mapSize = 5;
 int currentPosition[] = {0, 0};
+char directionFacing[] = "right";
+
+// Functions Initialization
+void mapLayout();
+void movePlayer(char movement, int mazeMap[][mapSize], int currentPosition[2]);
 
 int main(){
-    int mazeMap[5][5] = {   {1, 1, 1, 1, 1},
-                            {0, 0, 0, 0, 1},
-                            {1, 1, 1, 1 ,1},
-                            {1, 0, 0, 0, 0},
-                            {1, 1, 1, 1, 1}          };
-    int finishLine[] = {4, 4};
+    int mazeMap[5][5] = {   {1, 2, 2, 2, 2},
+                            {0, 0, 0, 0, 2},
+                            {2, 2, 2, 2 ,2},
+                            {2, 0, 0, 0, 0},
+                            {2, 2, 2, 2, 2}          };
+    mapSize = 5;
 
     while(1){
-        printf("\e[1;1H\e[2J");
+        //printf("\e[1;1H\e[2J");
         mapLayout(mazeMap, currentPosition, sizeof(mazeMap) / sizeof(mazeMap[0]), sizeof(mazeMap[0])/ sizeof(mazeMap[0][0]));
-        printf("\nCurrent Y-Position: %d\nCurrent X-Position: %d\nYour Move: ", currentPosition[0], currentPosition[1]);
+        printf("\nCurrent Y-Position: %d\t\t[w] - Move Forward\nCurrent X-Position: %d\t\t[s] - Move Backward\nDirection Facing: %s\t\t[a / d] - Rotate\nYour Move: ", currentPosition[0], currentPosition[1], directionFacing);
         char movement;
         scanf("%c", &movement);
         movePlayer(movement, mazeMap, currentPosition);
-        if(currentPosition[0] == finishLine[0] && currentPosition[1] == finishLine[1]) break;
     }
 }
 
-void movePlayer(char movement, int mazeMap[][5], int currentPosition[2]){
-    int Position[] = {currentPosition[0], currentPosition[1]};
-    if(movement == 'w') Position[0] -= 1;
-    if(movement == 'a') Position[1] -= 1;
-    if(movement == 's') Position[0] += 1;
-    if(movement == 'd') Position[1] += 1;
+void movePlayer(char movement, int mazeMap[][mapSize], int currentPosition[2]){
+    char *directions[] = {"up", "right", "down", "left"};
+    int directionIndex;
+    for(int i = 0; i < 4; i++) if(!strcmp(directionFacing, directions[i])) directionIndex = i;
+    
+    int newY = currentPosition[0];
+    int newX = currentPosition[1];
 
-    if(mazeMap[Position[0]][Position[1]] != 0){
-        currentPosition[0] = Position[0];
-        currentPosition[1] = Position[1];
+    if(movement == 'a'){                                                // Rotate Counter Clockwise
+        directionIndex = (directionIndex + 3) % 4;
+        strcpy(directionFacing, directions[directionIndex]);
+    }
+    else if(movement == 'd'){                                           // Rotate Clockwise
+        directionIndex = (directionIndex + 1) % 4;
+        strcpy(directionFacing, directions[directionIndex]);
+    }
+    else if(movement == 'w' || movement == 's'){                        // Move Forward and Backward
+        int direction = (movement == 'w') ? -1 : 1;
+
+        if(!strcmp(directionFacing, "up")) newY += direction;
+        else if(!strcmp(directionFacing, "right")) newX -= direction;
+        else if(!strcmp(directionFacing, "down")) newY -= direction;
+        else if(!strcmp(directionFacing, "left")) newX += direction;
+    }
+
+    // Updates the Current Position from the Maze
+    if(mazeMap[newY][newX] != 0){
+        currentPosition[0] = newY;
+        currentPosition[1] = newX;
+
+        // Makes it a blank tile
+        mazeMap[newY][newX] = 1;
     }
 }
 
-void printHorizontalLines(int columns){
-    for(int i = 0; i < columns; i++) printf("+-------");
-    printf("+\n");
-}
-
-void printCell(int mazeMap[][5], int currentPosition[2], int Yaxis, int Xaxis){
-    if(Yaxis == currentPosition[0] && Xaxis == currentPosition[1]) printf(" O ");       // Player
-    else printf(mazeMap[Yaxis][Xaxis] == 1 ? "   " : " X ");                            // Path and Blocked Paths
-}
-
-void mapLayout(int mazeMap[][5], int currentPosition[2], int row, int column){
+void mapLayout(int mazeMap[][mapSize], int currentPosition[2], int row, int column){
+    int columnSpacing, columnSeparator, rowSeparator, rowLines;
+    int Xaxis, Yaxis;
     printf("\n");
-
-    // Print Top Border
-    printHorizontalLines(column);
-
-    // Print Maze Rows
-    for(int Yaxis = 0; Yaxis < row; Yaxis++){
-        // Print Maze Cells and Vertical Borders
-        for(int rowSeparator = 0; rowSeparator < 3; rowSeparator++){
-            printf("\t|");
-            for(int Xaxis = 0; Xaxis < column; Xaxis++){
-                printCell(mazeMap, currentPosition, Yaxis, Xaxis);
+    for(Yaxis = 0; Yaxis < row; Yaxis++){
+        // Vertical Lines
+        printf("\n\t");
+        for(rowSeparator = 0; rowSeparator < 5; rowSeparator++) printf("+-------");
+        printf("+");
+        // Horizontal Lines
+        for(columnSeparator = 0; columnSeparator < 3; columnSeparator++){
+            printf("\n\t");
+            for(Xaxis = 0; Xaxis < (column + 1); Xaxis++){
                 printf("|");
+                for(columnSpacing = 0; columnSpacing < 7; columnSpacing++){
+                    if(columnSpacing == 3 && columnSeparator == 1 && Xaxis != 5){
+                        if(Yaxis == currentPosition[0] && Xaxis == currentPosition[1]){                     // Player Icon
+                            if(!strcmp(directionFacing, "up")) printf("↑");
+                            if(!strcmp(directionFacing, "left")) printf("←");
+                            if(!strcmp(directionFacing, "down")) printf("↓");
+                            if(!strcmp(directionFacing, "right")) printf("→");
+                        }
+                        else printf(mazeMap[Yaxis][Xaxis] == 1 ? " " : (mazeMap[Yaxis][Xaxis] == 0) ? "X" : "◦");                                // Open and Blocked Paths
+                    }
+                    else printf(" ");
+                }
             }
-            printf("\n");
-
-            // After Printing Cells, Print Horizontal Separator
-            if(rowSeparator == 2) printHorizontalLines(column);
         }
     }
+    printf("\n\t");
+    for(rowSeparator = 0; rowSeparator < 5; rowSeparator++) printf("+-------");
+    printf("+");
 }
